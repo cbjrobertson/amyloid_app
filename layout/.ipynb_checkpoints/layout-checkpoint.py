da0eb2891e35aa60ead2bb4313f3aa9f95d660ab
Cole Robertson <cbjrobertson@gmail.com>
2023-11-30 12:58:43 -0500

    update ignore

--- layout/.ipynb_checkpoints/layout-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from dash import  html
-from dash import dcc
-
-from layout.sidebar.sidebar import sidebar
-
-
-content = html.Div(id="page-content")
-
-layout = html.Div([dcc.Location(id="url"), sidebar, content])
--- utils/.ipynb_checkpoints/constants-checkpoint.py
+++
@@ -1,5 +0,0 @@
-scatter_3d_page_location = "/"
-barword_page_location = "/barword"
-barpos_page_location = "/barpos"
-
-TIMEOUT = 60
